1c5f9d3cc9a07b71aa7bf7a2b1fab74dae2a7b7a
sebastian-sz <58402418+sebastian-sz@users.noreply.github.com>
2020-11-26 20:14:49 +0100

    Refactored documentation.

--- projectpath/project_path.go
+++ projectpath/project_path.go
@@ -1,3 +1,10 @@
+// This is a utility package for managing relative / absolute file paths used in this repository.
+// For example dbconnectors.JSONDataConnector uses data stored in "data/otwock_db.json". If we pass this path
+// directly, we can get IO Errors depending on the directory from which we start the GotwockAppServer (assuming that
+// dbconnectors.JSONDataConnector is used).
+// In order to fix this issue one can invoke GetRootPath and join the result with relative path from the directory.
+// That way components like dbconnectors.JSONDataConnector are going to receive full, absolute path and it will be
+// possible to run the entire app from different directories.
 package projectpath
 
 import (
